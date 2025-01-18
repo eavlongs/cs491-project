01c679724a35bbf1041c69a66336085c76e96fad
Bunlong-Prak <100819214+Bunlong-Prak@users.noreply.github.com>
2025-01-18 11:43:34 +0700

    Minor changes: increase the padding for the date-box component

--- theater/front/src/app/admin/display-movie/page.tsx
+++ theater/front/src/app/admin/display-movie/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import { useState } from 'react'
+
 import { Movie } from '@/components/custom/movies'
-import { DateSelector } from '@/components/custom/date-box'
 
 export default function Page() {
-    const [, setSelectedDate] = useState<string>('25 Dec')
 
     return (
         <div className="container mx-auto py-6 space-y-6">
-            <DateSelector onDateSelect={setSelectedDate} />
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {Array.from({ length: 6 }).map((_, index) => (
