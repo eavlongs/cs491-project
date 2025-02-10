efae065e37d2435b259f992abf379605fb866473
Bunlong-Prak <100819214+Bunlong-Prak@users.noreply.github.com>
2025-02-10 22:28:13 +0700

    put display-listing-movie as inside (user) and  add EditMovieForm

---
+++ subscription/front/src/app/(user)/page.tsx
@@ -0,0 +1,15 @@
+import { Movie } from '@/components/custom/Movie'
+import { getMovies } from './actions'
+
+export default async function Page() {
+    const movies = await getMovies()
+    return (
+        <div className="min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+            <div className="grid auto-rows-min gap-6 md:grid-cols-3 items-center">
+                {movies.map((movie) => (
+                    <Movie movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    )
+}
