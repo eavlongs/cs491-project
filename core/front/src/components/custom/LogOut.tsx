d597029f36b5801c93a243d1a1cc0420e784de2f
Bunlong-Prak <100819214+Bunlong-Prak@users.noreply.github.com>
2025-02-14 22:47:25 +0700

    display user name when logged in, it is inside LogOut  component

--- core/front/src/components/custom/LogOut.tsx
+++ core/front/src/components/custom/LogOut.tsx
@@ -1,19 +1,26 @@
 'use client'
+
+// import { signOut, useSession } from "next-auth/react"
+import { Button } from '@/components/ui/button'
 import { signOut } from 'next-auth/react'
-import { Button } from '../ui/button'
 
 export function LogOut() {
+    //   const { data: session } = useSession()
+
     return (
-        <Button
-            variant="destructive"
-            onClick={async () =>
-                signOut({
-                    callbackUrl: '/',
-                    redirect: true,
-                })
-            }
-        >
-            Log Out
-        </Button>
+        <div className="flex items-center gap-4">
+            {/* {session?.user?.name && <span className="text-sm font-medium">{session.user.name}</span>} */}
+            <Button
+                variant="destructive"
+                onClick={async () =>
+                    signOut({
+                        callbackUrl: '/',
+                        redirect: true,
+                    })
+                }
+            >
+                Log Out
+            </Button>
+        </div>
     )
 }
